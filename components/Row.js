99507d1c2f42215c38bd491f90f97d6c289f5939
Hans Meyer <ham@hansifer.com>
2023-10-29 13:00:18 -0600

    make committed non-matching tile bg color even more apparent

--- components/Row.js
+++ components/Row.js
@@ -17,7 +17,7 @@ export const Row = ({ active, word = "", guessWord }) => {
       nonGreenChars.splice(nonGreenChars.indexOf(char), 1);
       return "#b69f3c"; // yellow
     }
-    return "rgb(39 39 42)";
+    return "#3a3a3c";
   };
 
   return (
