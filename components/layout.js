3cea5314ca17f266430a03dfea9becd00a2da9b1
Hans Meyer <ham@hansifer.com>
2023-10-29 15:41:15 -0600

    update site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Wordle";
-export const siteTitle = "Wordle";
+export const siteTitle = "Free and Infinite Wordle Practice";
 
 export default function Layout({ children, home }) {
   return (
